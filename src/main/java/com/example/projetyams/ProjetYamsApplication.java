fb0dbecac2c30d3a7f1d4cdbdc094967ab47ebef
AlexisMalattia <alexis.malattia06@gmail.com>
2023-04-14 16:22:47 +0200

    Initialisation

---
+++ src/main/java/com/example/projetyams/ProjetYamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetyams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetYamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetYamsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projetyams/ProjetYamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetyams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetYamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
