beafa664258ca9c88e501824addac8df1ba5ab87
Alexis Malattia <alexis.malattia@atos.net>
2023-04-26 20:38:57 +0200

    Premières corrections

--- src/main/java/com/example/projetyams/joueur/JoueurApp.java
+++ src/main/java/com/example/projetyams/joueur/JoueurApp.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JoueurApp {
     public static void main(String[] args) {
+        int port = 8081;
+        String url = "http://localhost:" + port+"/joueur";
+        System.setProperty("joueur.url", url);
         SpringApplication.run(JoueurApp.class, args);
     }
 }
--- src/main/java/com/example/projetyams/joueur/controller/JoueurController.java
+++ src/main/java/com/example/projetyams/joueur/controller/JoueurController.java
@@ -3,36 +3,21 @@ package com.example.projetyams.joueur.controller;
 import com.example.projetyams.joueur.service.JoueurService;
 import com.example.projetyams.joueur.model.Joueur;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/joueur/{id}")
+@RequestMapping("/joueur")
 public class JoueurController {
     @Autowired
     private JoueurService joueurService;
 
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Joueur> getJoueur() {
-//        return ResponseEntity.ok(joueurService.getJoueur(id));
-//    }
-//    @PostMapping
-//    public ResponseEntity<Joueur> createJoueur(Joueur joueur) {
-//        return ResponseEntity.ok(joueurService.createJoueur(joueur));
-//    }
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Joueur> deleteJoueur(Long id) {
-//        return ResponseEntity.ok(joueurService.deleteJoueur(id));
-//    }
-
-    /**
-     * Exemple de joueur en dur pour créer le service, on utilisera la version en commentaire plus haut pour le projet
-     */
     private Joueur joueur = new Joueur("joueurTest", (long) (Math.random() * 1000));
-     @GetMapping
-        public ResponseEntity<Joueur> getJoueur() {
-            return ResponseEntity.ok(joueur);
-        }
+    @GetMapping
+    public Joueur getJoueur(){
+        return joueur;
+    }
      @PutMapping
         public ResponseEntity<Joueur> updateJoueur(@RequestBody Joueur joueurUpdated) {
              joueur.setNom(joueurUpdated.getNom());
