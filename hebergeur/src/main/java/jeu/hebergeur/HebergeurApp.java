53494a6992915658a3e452ca520cde1ea6bf3be4
yassine BERRIRI <yassineberriri22@gmail.com>
2023-05-27 21:31:37 +0200

    Comment the Docker configuration in JoueurApp and HeberegeurApp

--- hebergeur/src/main/java/jeu/hebergeur/HebergeurApp.java
+++ hebergeur/src/main/java/jeu/hebergeur/HebergeurApp.java
@@ -17,6 +17,26 @@ public class HebergeurApp {
     @Bean
     public CommandLineRunner initialisation(Hebergeur hebergeur, WebClient.Builder builder){
         return args -> {
+            System.out.println("args" + args);
+            //hebergeur.setNbJoueurMax(3);
+            hebergeur.setNbJoueurMax(2);
+            hebergeur.setNbPartie(0);
+            String url = "http://localhost:";
+            String port =args[0].split("=")[1];
+            url += port + "/hebergeur";
+            System.out.println(url);
+            WebClient webClient = builder.baseUrl(url).build();
+            webClient.post().uri("http://localhost:8080/appariement/hebergeurs")
+                    .bodyValue(url).retrieve()
+                    .bodyToMono(Void.class)
+                    .block();
+
+            /**
+             * Docker
+             */
+
+
+            /*
             hebergeur.setNbJoueurMax(2);
             hebergeur.setNbPartie(0);
             if (args.length > 0 ){
@@ -32,6 +52,13 @@ public class HebergeurApp {
                             .block();
                 }
             }
+             */
+
+            /**
+             * Docker
+             */
         };
+
+
     }
 }
--- joueur/src/main/java/jeu/joueur/JoueurApp.java
+++ joueur/src/main/java/jeu/joueur/JoueurApp.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.net.InetAddress;
+
 @SpringBootApplication
 public class JoueurApp {
 
@@ -19,24 +21,50 @@ public class JoueurApp {
     @Bean
     public CommandLineRunner initialisation(Joueur joueur, WebClient.Builder builder){
         return args -> {
+
+            joueur.setNom("Joueur 1");
+            String localhost = "http://localhost:";
+            String port =args[0].split("=")[1];
+            String url = localhost + port + "/joueur";
+            WebClient webClient = builder.baseUrl(url).build();
+            webClient.post().uri("http://localhost:8080/appariement/joueurs")
+                    .bodyValue(url).retrieve()
+                    .bodyToMono(Void.class)
+                    .block();
+
+
+
+
+            /**
+             * Docker
+             */
+            /*
             System.out.println(args.length);
             joueur.setNom("Joueur 1");
             if (args.length > 0) {
 
-                String localhost = "http://localhost:";
-                String[] parts = args[0].split("=");
-                if (parts.length > 1) {
-                    port =/*args[0].split("=")*/parts[1];
-                    String url = localhost + port + "/joueur";
+                if (args.length > 1) {
+                    port =args[1];
 
-                    WebClient webClient = builder.baseUrl(url).build();
-                    webClient.post().uri("http://localhost:8080/appariement/joueurs")
-                            .bodyValue(url).retrieve()
+                    String myIp = InetAddress.getLocalHost().getHostAddress();
+                    String myUrl = "http://"+myIp+":"+port;
+
+                    WebClient webClient = builder.baseUrl(port).build();
+                    System.out.println("builder.baseUrl(port).build()" + builder.baseUrl(port).build());
+                    webClient.post().uri(myUrl+"/appariement/joueurs")
+                            .bodyValue(port).retrieve()
                             .bodyToMono(Void.class)
                             .block();
                 }
 
             }
+
+             */
+
+            /**
+             * Docker
+             */
+
         };
     }
 }
